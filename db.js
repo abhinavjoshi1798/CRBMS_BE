79258861cfaeceb4d4a52a88c219c6faec442d27
Abhinav Joshi <abhinavjoshi1798@gmail.com>
2024-01-22 11:53:39 +0530

    mongodb atlas added

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-const connection = mongoose.connect("mongodb://localhost:27017/crbms");
+// const connection = mongoose.connect("mongodb://localhost:27017/crbms");
+const connection = mongoose.connect("mongodb+srv://abhinavjoshi1798:candyman@cluster0.ztbnruv.mongodb.net/?retryWrites=true&w=majority");
 
 module.exports = {
     connection
