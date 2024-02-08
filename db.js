29ffde7165c94552d3a8445cb57bf7de94ffdd48
Abhinav Joshi <abhinavjoshi1798@gmail.com>
2024-02-08 13:56:08 +0530

    mongoDbAtlas

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(
-  // "mongodb+srv://abhinavjoshi1798:candyman@cluster0.ztbnruv.mongodb.net/?retryWrites=true&w=majority"
-  "mongodb://localhost:27017/crbms"
-);
+const connection = mongoose.connect("mongodb+srv://abhinavjoshi1798:candyman@cluster0.ztbnruv.mongodb.net/CRBMS?retryWrites=true&w=majority");
 
 module.exports = {
   connection,
 };
+
